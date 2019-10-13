a66e1892c05f66361e291716cce84aab776cdc7f
yaoyaoz <905440208@qq.com>
2019-10-13 12:58:28 +0800

    通讯录：添加联系人

--- 通讯录管理系统/通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统/通讯录管理系统.cpp
@@ -3,6 +3,93 @@
 */
 #include<iostream>
 using namespace std;
+#include<string>
+
+#define MAX 1000
+
+//设计联系人结构体
+struct Person
+{
+	string m_Name;
+	int m_Sex;//1 男	2 女
+	int m_Age;
+	string m_Phone;
+};
+
+//设计通讯录结构体
+struct Addressbooks
+{
+	struct Person personArray[MAX];
+	
+	//通讯录中当前记录联系人个数
+	int m_Size;
+};
+
+//1、添加联系人
+void addPerson(Addressbooks * abs)
+{
+	if (abs->m_Size == MAX)
+	{
+		cout << "通讯录已满，无法添加！" << endl;
+	}
+	else
+	{
+		//添加具体联系人
+		
+		//姓名
+		string name;
+		cout << "请输入姓名：" << endl;
+		cin >> name;
+		abs->personArray[abs->m_Size].m_Name = name;
+
+		//性别
+		int sex = 0;
+		cout << "请输入性别：" << endl;
+		cout << "1 --- 男" << endl;
+		cout << "2 --- 女" << endl;
+		while (true)
+		{
+			cin >> sex;
+			if (sex == 1 || sex == 2)
+			{
+				abs->personArray[abs->m_Size].m_Sex = sex;
+				break;
+			}
+			cout << "输入有误，请重新输入" << endl;
+		}
+
+		//年龄
+		cout << "请输入年龄：" << endl;
+		int age = 0;
+		while (true)
+		{
+			cin >> age;
+			if (age > 0 && age << 150)
+			{
+				abs->personArray[abs->m_Size].m_Age = age;
+				break;
+			}
+			else
+			{
+				cout << "输入有误，请重新输入" << endl;
+			}
+		}
+
+		//电话
+		string phone;
+		cout << "请输入电话" << endl;
+		cin >> phone;
+		abs->personArray[abs->m_Size].m_Phone = phone;
+
+		//更新通讯录人数
+		abs->m_Size++;
+
+		cout << "添加成功" << endl;
+
+		system("pause"); //请按任意键继续
+		system("cls"); //清屏操作
+	}
+}
 
 //菜单界面
 void showMenu()
@@ -20,6 +107,11 @@ void showMenu()
 
 int main()
 {
+	//创建通讯录结构体变量
+	Addressbooks abs;
+	//初始化通讯录中当前人员个数
+	abs.m_Size = 0;
+
 	int select = 0; //创建用户选择输入的变量
 
 	while (true) {
@@ -31,6 +123,7 @@ int main()
 		{
 		case 1:	//1、添加联系人
 			cout << "添加联系人了哦" << endl;
+			addPerson(&abs);//利用地址传递，可以修改实参
 			break;
 		case 2: //2、显示联系人
 			break;
