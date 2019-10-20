2363023692e767809343d4c33506cdf3706d6ad0
yaoyaoz <905440208@qq.com>
2019-10-20 23:11:00 +0800

    通讯录：修改、查找联系人

--- 通讯录管理系统/通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统/通讯录管理系统.cpp
@@ -91,6 +91,96 @@ void addPerson(Addressbooks * abs)
 	}
 }
 
+//2、显示所有联系人
+void showPerson(Addressbooks * abs)
+{
+	//判断通讯录中人数是否为0，如果为0，提示记录为空
+	//如果不为0，显示记录的联系人信息
+	if (abs->m_Size == 0)
+	{
+		cout << "当前记录为空" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < abs->m_Size; i++)
+		{
+			cout << "姓名：" << abs->personArray[i].m_Name << "\t";
+			cout << "性别：" << (abs->personArray[i].m_Sex == 1 ? "男" : "女") << "\t";
+			cout << "年龄：" << abs->personArray[i].m_Age << "\t";
+			cout << "电话：" << abs->personArray[i].m_Phone << "\t\n";
+		}
+	}
+
+	system("pause");//请按任意键继续
+	system("cls");//清屏
+}
+
+//检测联系人是否存在，如果存在，返回联系人所在数组中的具体位置，不存在返回-1
+//参数1：通讯录	参数2：对比的姓名
+int isExist(Addressbooks * abs, string name)
+{
+	for (int i = 0; i < abs->m_Size; i++)
+	{
+		if (abs->personArray[i].m_Name == name)
+		{
+			return i;//找到了，返回这个人在数组中的下标编号
+		}
+	}
+	return -1;//如果遍历结束都没有找到，返回-1
+}
+
+//3、删除指定联系人
+void deletePerson(Addressbooks * abs)
+{
+	cout << "请输入删除联系人性名：" << endl;
+	string name;
+	cin >> name;
+	int ret = isExist(abs, name);
+	if (ret == -1)
+	{
+		cout << "查无此人" << endl;
+	}
+	else
+	{
+		//查找到人，要进行删除操作
+		for (int i = ret; i < abs->m_Size; i++)
+		{
+			//数据前移
+
+			abs->personArray[i] = abs->personArray[i + 1];
+		}
+		abs->m_Size--;//更新通讯录中的人员数
+		cout << "删除成功" << endl;
+	}
+	system("pause");
+	system("cls");
+}
+
+//4、查找指定联系人信息
+void findPerson(Addressbooks * abs)
+{
+	cout << "请输入您要查找的联系人" << endl;
+	string name;
+	cin >> name;
+
+	//判断指定的联系人是否存在通讯录中
+	int ret = isExist(abs, name);
+
+	if (ret != -1)	//找到联系人
+	{
+		cout << "姓名：" << abs->personArray[ret].m_Name << "\t";
+		cout << "性别：" << (abs->personArray[ret].m_Sex == 1 ? "男" : "女") << "\t";
+		cout << "年龄：" << abs->personArray[ret].m_Age << "\t";
+		cout << "电话：" << abs->personArray[ret].m_Phone << "\t\n";
+	}
+	else
+	{
+		cout << "查无此人" << endl;
+	}
+	system("pause");
+	system("cls");
+}
+
 //菜单界面
 void showMenu()
 {
@@ -122,14 +212,30 @@ int main()
 		switch (select)
 		{
 		case 1:	//1、添加联系人
-			cout << "添加联系人了哦" << endl;
 			addPerson(&abs);//利用地址传递，可以修改实参
 			break;
 		case 2: //2、显示联系人
+			showPerson(&abs);
 			break;
 		case 3: //3、删除联系人
+		//{}代表代码块，如果不加{},switch那里会提示语法错误
+		/*{
+			cout << "请输入删除联系人性名：" << endl;
+			string name;
+			cin >> name;
+			if (isExist(&abs, name) == -1)
+			{
+				cout << "查无此人" << endl;
+			}
+			else
+			{
+				cout << "找到此人" << endl;
+			}
+		}*/
+			deletePerson(&abs);
 			break;
 		case 4: //4、查找联系人
+			findPerson(&abs);
 			break;
 		case 5: //5、修改联系人
 			break;
