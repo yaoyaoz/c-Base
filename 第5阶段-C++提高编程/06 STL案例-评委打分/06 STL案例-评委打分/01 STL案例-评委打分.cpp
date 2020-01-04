c003c51266d68e21e8888732eb8412e4f92a8452
yaoyaoz <905440208@qq.com>
2020-01-04 22:57:58 +0800

    第5阶段-C++提高编程：STL案例-评委打分

---
+++ 第5阶段-C++提高编程/06 STL案例-评委打分/06 STL案例-评委打分/01 STL案例-评委打分.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<deque>
+#include<string>
+#include<algorithm>
+#include<ctime>
+
+/*
+需求：
+有5名选手：选手ABCDE，10个评委分别对每一名选手打分，去除最高分，去除评委中最低分，取平均分
+*/
+
+//选手类
+class Person
+{
+public:
+	Person(string name, int score)
+	{
+		this->m_Name = name;
+		this->m_Score = score;
+	}
+	string m_Name;
+	int m_Score; //平均分
+};
+
+void printVector(vector<Person>& v)
+{
+	for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		//cout << "姓名：" << (*it).m_Name << " 分数：" << (*it).m_Score << endl;
+		cout << "姓名：" << it->m_Name << " 分数：" << it->m_Score << endl;
+	}
+}
+
+void createPerson(vector<Person>& v)
+{
+	string nameSeed = "ABCDE";
+	for (int i = 0; i < 5; i++)
+	{
+		string name = "选手";
+		name += nameSeed[i];
+
+		int score = 0;
+
+		Person p(name, score);
+
+		//将创建的person对象放入到容器中
+		v.push_back(p);
+	}
+}
+
+//打分
+void setScore(vector<Person>& v)
+{
+	for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		//将评委的分数放入到deque容器中
+		deque<int> d;
+		for (int i = 0; i < 10; i++)
+		{
+			int score = rand() % 41 + 60;  //60~100
+			d.push_back(score);
+		}
+
+		//排序
+		sort(d.begin(), d.end());
+
+		//去除最高和最低分
+		d.pop_front();
+		d.pop_back();
+
+		//取平均分
+		int sum = 0;
+		cout << "选手：" << it->m_Name << " 打分：" << endl;
+		for (deque<int>::iterator dit = d.begin(); dit != d.end(); dit++)
+		{
+			cout << *dit << " ";
+			sum += *dit; //累加每个评委的分数
+		}
+		cout << endl;
+
+		int avg = sum / d.size();
+
+		//将平均分赋值给选手身上
+		it->m_Score = avg;
+	}
+}
+
+int main()
+{
+	//随机数种子
+	srand((unsigned int)time(NULL)); //如果不加这句话，每次运行得到的随机数都是一样的
+
+	cout << "1、创建5名选手=>" << endl;
+	vector<Person> v;
+	createPerson(v);
+	printVector(v);
+
+	cout << "\n2、给5名选手打分=>" << endl;
+	setScore(v);
+
+	cout << "\n3、显示最后得分=>" << endl;
+	printVector(v);
+
+	system("pause");
+	return 0;
+}
